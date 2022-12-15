b01514806a6dacadb00287d0b4baf660ee655bc4
ko-shiro-hap <koushirou.iino@icloud.com>
2022-12-16 00:00:58 +0900

    削除機能の作成

--- src/services/delete_question.php
+++ src/services/delete_question.php
@@ -4,5 +4,21 @@ declare(strict_types = 1);
 // PDOの設定を呼び出す
 require('../dbconnect.php');
 
-echo $_POST['id'];
+if(isset($_POST['id'])) {
+  $id = $_POST['id'];
+
+  try {
+    $sql = "DELETE FROM questions WHERE id = :id";
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindParam(":id", $id);
+    $stmt->execute();
+
+    header('Location: ../admin/index.php');
+    exit();
+
+  } catch (PDOException $e) {
+    echo $e->getMessage();
+    die();
+  }
+}
 ?>
